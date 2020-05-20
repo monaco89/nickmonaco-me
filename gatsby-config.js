713f783695b3b76af4de2abc58bcf87c4cb72a21
Nick Monaco <nick.monaco@icf.com>
2020-05-20 14:49:35 -0400

    remove policies

--- gatsby-config.js
+++ gatsby-config.js
@@ -43,23 +43,23 @@ module.exports = {
         ],
       },
     },
-    {
-      resolve: `gatsby-plugin-csp`,
-      options: {
-        disableOnDev: true,
-        reportOnly: false, // Changes header to Content-Security-Policy-Report-Only for csp testing purposes
-        mergeScriptHashes: true, // you can disable scripts sha256 hashes
-        mergeStyleHashes: true, // you can disable styles sha256 hashes
-        mergeDefaultDirectives: true,
-        directives: {
-          "script-src": "'self' www.google-analytics.com",
-          // TODO
-          // "style-src": "'self' 'unsafe-inline'",
-          "img-src": "'self' data: www.google-analytics.com",
-          // you can add your directives or override defaults
-        },
-      },
-    },
+    // {
+    //   resolve: `gatsby-plugin-csp`,
+    //   options: {
+    //     disableOnDev: true,
+    //     reportOnly: false, // Changes header to Content-Security-Policy-Report-Only for csp testing purposes
+    //     mergeScriptHashes: true, // you can disable scripts sha256 hashes
+    //     mergeStyleHashes: true, // you can disable styles sha256 hashes
+    //     mergeDefaultDirectives: true,
+    //     directives: {
+    //       "script-src": "'self' www.google-analytics.com",
+    //       // TODO
+    //       // "style-src": "'self' 'unsafe-inline'",
+    //       "img-src": "'self' data: www.google-analytics.com",
+    //       // you can add your directives or override defaults
+    //     },
+    //   },
+    // },
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
