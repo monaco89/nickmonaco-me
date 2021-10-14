11020f7fe3a8664f5eb0af8122b33d301c6b014a
monaco89 <nick.monaco15@gmail.com>
2021-10-14 12:22:32 -0400

    remove isMobile hook

--- src/pages/fm.js
+++ src/pages/fm.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
-import Cassette from '../components/CassettePlayer';
+// import Cassette from '../components/CassettePlayer';
 import TurnTable from '../components/TurnTable';
-import useIsMobile from '../utils/useIsMobile';
 
 function FmPage({ path }) {
-  const isMobile = useIsMobile();
-
   return (
     <Layout path={path}>
       <SEO title="🔈" keywords={[`nick`, `monaco`, `music`, `fm`]} />
-      {isMobile ? <Cassette /> : <TurnTable />}
+      <TurnTable />
     </Layout>
   );
 }
