4c34bc751b78af8b6bf49342740262b93d2e6d11
monaco89 <nick.monaco15@gmail.com>
2021-10-14 10:07:49 -0400

    check for window

--- src/utils/useIsMobile.js
+++ src/utils/useIsMobile.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
 function useIsMobile() {
-  const [width, setWidth] = React.useState(window.innerWidth);
+  const [width, setWidth] = React.useState(window && window.innerWidth);
   function handleWindowSizeChange() {
-    setWidth(window.innerWidth);
+    setWidth(window && window.innerWidth);
   }
   React.useEffect(() => {
     window.addEventListener('resize', handleWindowSizeChange);
