c59ad6b5a8d8b1ab59938c40773092bae45dcf91
monaco89 <nick.monaco15@gmail.com>
2021-10-23 20:37:07 -0400

    get width and height

--- src/utils/useIsMobile.js
+++ src/utils/useIsMobile.js
@@ -1,12 +1,26 @@
 import React from 'react';
 
 function useIsMobile() {
-  const [width, setWidth] = React.useState(
-    typeof window !== undefined ? window.innerWidth : null
-  );
+  // define dimension variables for use in hook
+  let height;
+  let width;
+  // Check for the window element.
+  // Needed for server size rendering of Gatsby else use of window.innerHeight/Width will cause build failures.
+  if (typeof window !== `undefined`) {
+    height = window.innerHeight;
+    width = window.innerWidth;
+  }
+  // See default state of dimensions
+  const [dimensions, setDimensions] = React.useState({
+    windowHeight: height,
+    windowWidth: width,
+  });
 
   function handleWindowSizeChange() {
-    setWidth(typeof window !== undefined ? window.innerWidth : null);
+    setDimensions({
+      windowHeight: window.innerHeight,
+      windowWidth: window.innerWidth,
+    });
   }
 
   React.useEffect(() => {
@@ -16,7 +30,7 @@ function useIsMobile() {
     };
   }, []);
 
-  const isMobile = width <= 768;
+  const isMobile = dimensions.width <= 768;
 
   return isMobile;
 }
